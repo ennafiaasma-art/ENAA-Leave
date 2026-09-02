25149456afac93cf2ec7496f29cb284e38bf7975
ennafiaasma-art <ennafiaasma@gmail.com>
2026-09-02 22:48:22 +0100

    la creation de demmande conge route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DemandeCongeController;
+
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/demandes-conge', [DemandeCongeController::class, 'index']);
+    Route::get('/demandes-conge/{id}', [DemandeCongeController::class, 'show']);
+    Route::post('/demandes-conge', [DemandeCongeController::class, 'store']);
+    Route::put('/demandes-conge/{id}', [DemandeCongeController::class, 'update']);
+    Route::delete('/demandes-conge/{id}', [DemandeCongeController::class, 'destroy']);
 });
