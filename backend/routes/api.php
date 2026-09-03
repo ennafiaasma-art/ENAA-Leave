bf74d47629af219f08d848f8b77e91c3a7815257
ennafiaasma-art <ennafiaasma@gmail.com>
2026-09-03 10:20:10 +0100

    corriger quelque error de route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,21 +1,13 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\DemandeCongeController;
 use App\Http\Controllers\Api\TypeCongeController;
+use App\Http\Controllers\Api\DemandeCongeController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/demandes-conge', [DemandeCongeController::class, 'index']);
-    Route::get('/demandes-conge/{id}', [DemandeCongeController::class, 'show']);
-    Route::post('/demandes-conge', [DemandeCongeController::class, 'store']);
-    Route::put('/demandes-conge/{id}', [DemandeCongeController::class, 'update']);
-    Route::delete('/demandes-conge/{id}', [DemandeCongeController::class, 'destroy']);
-});
-Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/types-conge', [TypeCongeController::class, 'index']);
     Route::get('/types-conge/{id}', [TypeCongeController::class, 'show']);
@@ -23,4 +15,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/types-conge/{id}', [TypeCongeController::class, 'update']);
     Route::delete('/types-conge/{id}', [TypeCongeController::class, 'destroy']);
 
+    Route::get('/demandes-conge', [DemandeCongeController::class, 'index']);
+    Route::get('/demandes-conge/{id}', [DemandeCongeController::class, 'show']);
+    Route::post('/demandes-conge', [DemandeCongeController::class, 'store']);
+    Route::put('/demandes-conge/{id}', [DemandeCongeController::class, 'update']);
+    Route::delete('/demandes-conge/{id}', [DemandeCongeController::class, 'destroy']);
 });
