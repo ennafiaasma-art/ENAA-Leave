64b321ade3739fe795d2d6c3e50158955339dacb
ennafiaasma-art <ennafiaasma@gmail.com>
2026-09-04 13:04:31 +0100

    modification de cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,11 +2,17 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
-'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
